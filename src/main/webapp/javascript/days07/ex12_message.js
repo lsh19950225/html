c528e0042da3e27823de86dd3f995ceb5304456c
lsh <leesh950225@naver.com>
2024-10-08 17:35:00 +0900

    js_days07

---
+++ src/main/webapp/javascript/days07/ex12_message.js
@@ -0,0 +1,12 @@
+/**
+ * ex12_message.js
+ 	함수 내보내기
+ */
+ 
+ const message = ()=>{
+ 	const name = "홍길동";
+ 	const age = 20;
+ 	return `${name} / ${age}`;
+ };
+ 
+ export default message;
---
+++ src/main/webapp/javascript/days07/ex12_person.js
@@ -0,0 +1,14 @@
+/**
+ * ex12_person.js
+ 	변수 내보내기
+ */
+ 
+ /*
+ export const name = "admin";
+ export const age = 20;
+ */
+ 
+ /* 한번에 내보내기 */
+ const name = "admin";
+ const age = 20;
+ export{name, age};
